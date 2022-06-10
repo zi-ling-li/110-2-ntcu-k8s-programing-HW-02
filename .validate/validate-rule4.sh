b6b9160dbe70e6ae01352494af395822ef6ebd56
Jimmy Chuang <ogre0403@gmail.com>
2022-06-10 15:57:32 +0800

    fix: wait for svc deleted after 20 try

--- .validate/validate-rule4.sh
+++ .validate/validate-rule4.sh
@@ -11,18 +11,36 @@ kubectl delete deployments.apps ${deployment} >/dev/null  2>&1
 
 LABEL="ntcu-k8s=hw2"
 
-svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
+
+for i in {1..20}; do
+  sleep 1
+
+  svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
+  if [[ "$svc_num" -eq 0 ]]; then
+      break
+  fi
+
+  if [[ "$i" -eq 20 ]]; then
+      echo "client建立的svc數量 $svc_num 不正確"
+      exit 1
+  fi
+
+done
 
 
-if [[ "$svc_num" -ne 0 ]]; then
-    echo "client建立的svc數量 $svc_num 不正確"
-    exit 1
-fi
+for i in {1..20}; do
+  sleep 1
 
-deployment_num=`kubectl get deployment -l ${LABEL}  -o yaml | yq '.items | length'`
-if [[ "$deployment_num" -ne 0 ]]; then
-    echo "client建立的deployment 數量 $deployment_num 不正確"
-    exit 1
-fi
+  deployment_num=`kubectl get deployment -l ${LABEL}  -o yaml | yq '.items | length'`
+  if [[ "$deployment_num" -eq 0 ]]; then
+      break
+  fi
+
+  if [[ "$i" -eq 20 ]]; then
+      echo "client建立的deployment 數量 $deployment_num 不正確"
+      exit 1
+  fi
+done
+
 
 echo "........ PASS"
